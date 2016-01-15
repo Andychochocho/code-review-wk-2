eb8aa1c2e2c04ef62d29d50c11db82a7623d69d1
Andrew Cho <cho-andrew@hotmail.com>
2016-01-15 14:08:42 -0800

    update our js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready(function) {
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
 
+    var weather = $('select[name=weather]:checked').val();
+    var gender = $('input[name=gender]:checked').val();
+    var children = $('input[name=children]:checked').val();
+    var married = $('input[name=]:married').val();
+    var animal = $('input[name=animal]:checked').val();
+
+    if (weather === 'hot') {
+      $('#las-vegas').show();
+    }
+    else if (weather === 'cold') {
+      $('#alaska').show();
+    }
+    else if (weather === 'both')
+      $('#japan').show();
+    event.preventDefault();
+  })
 });
